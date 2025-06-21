5fba33945b10481dfc6249063a187ae3b17667e4
uly576 <uliatokman23@gmail.com>
2025-06-21 16:19:00 +0300

    Додано код бота, конфігурацію Render та .gitignore

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os
+import telebot
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привіт! Бот працює ✅")
+
+bot.polling()
